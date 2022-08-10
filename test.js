8821eb720e08529332ce30607d06df2facb0d68c
Pouya Shojaei <54838223+s-pouya-sh@users.noreply.github.com>
2022-08-10 13:31:33 +0430

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-
 const {Gateway , Wallets} = require('fabric-network')
 const CCP = require('./CCP')
 
@@ -10,14 +9,19 @@ const ccp = CCP.loadCCP();
 const gateway = new Gateway();
 
 await gateway.connect(ccp, {
-    identity: walletId, 
+    identity: walletId,
     wallet,
+    discovery: { enabled: false, asLocalhost: false }
 });
+console.log('Connected to network');
 const network = await gateway.getNetwork('auction');
+console.log('Found channel auction');
 const contract = network.getContract('auction');
+console.log('Found cc auction');
 
 const result = await contract.createTransaction('createAuction')
-    .submit(["A_004", "AUC_004", "Bike4", "Desc1", "Featurs1",  "1200" , "Image1" , "Image2" , "Image3" , "10" , "2022-10-05", "P_001"]);
+    .submit('A_005', 'AUC_004', 'Bike4', 'Desc1', 'Featurs1',  '1200' , 'Image1' , 'Image2' , 'Image3' , '10' , '2022-10-05', 'P_001');
+console.log('Done');
 }
 
 test('appuser');
